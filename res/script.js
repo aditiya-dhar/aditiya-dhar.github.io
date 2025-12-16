662b5959df9b7761c124fe5ea876a83f98ecf3cf
adi <tut25650@temple.edu>
2025-12-15 21:25:21 -0500

    added "work" page

--- res/script.js
+++ res/script.js
@@ -3,19 +3,23 @@
 // page content [SPA]
 const pages = {
     home: `
-        <h1><a href="https://aditiya-dhar.github.io" title="home" style="text-decoration: none; color: black;">adi</a></h1>
-  <h2>hello, world<h2>
-  </body>
-  <p>
-    <ul>
-      <li>I made HOoT's website: <a href="https://h0otu.github.io/">HOoT</a></li>
-      <li>Hackathon frontend draft: <a href="routu/index.html">rouTU OwlHacks2025</a></li>
-    </ul>
-  </p>
-  <br>
-  <h2>This page is under construction :)</h2>
-  <img class="background" src="https://upload.wikimedia.org/wikipedia/commons/9/9c/Lunch_atop_a_Skyscraper_-_Charles_Clyde_Ebbets.jpg" alt="construction" width="30%">
-      `
+    <p>
+    hello world!
+    </br>
+    I'm Aditiya Dhar, a computer science student.
+    </br></br>
+    This page is under construction :)
+    </p>
+      `,
+    work: `
+    <p>
+    Here's some of the stuff I've worked on:
+        <ul>
+            <li>I made HOoT's website: <a href="https://h0otu.github.io/">HOoT</a></li>
+            <li>Hackathon frontend draft: <a href="routu/index.html">rouTU OwlHacks2025</a></li>
+        </ul>
+    </p>
+    `
 };
 
 function renderPage(page) {
@@ -28,9 +32,9 @@ function renderPage(page) {
 }
 
 function handleNav(e) {
-    if (e.target.classList.contains('nav-link')) {
+    if (e.target.classList.contains('link')) {
         e.preventDefault();
-        const page = e.target.getAttribute('data-page');
+        const page = e.target.getAttribute('title');
         window.location.hash = page;
         renderPage(page);
         setActiveLink(page);
@@ -38,8 +42,8 @@ function handleNav(e) {
 }
 
 function setActiveLink(page) {
-    document.querySelectorAll('.nav-link').forEach(link => {
-        if (link.getAttribute('data-page') === page) {
+    document.querySelectorAll('.link').forEach(link => {
+        if (link.getAttribute('title') === page) {
             link.classList.add('active');
         } else {
             link.classList.remove('active');
@@ -53,7 +57,7 @@ function initSPA() {
     renderPage(page);
     setActiveLink(page);
     // Listen for nav clicks
-    document.querySelector('nav').addEventListener('click', handleNav);
+    document.querySelector('link').addEventListener('click', handleNav);
     // Listen for hash changes (back/forward)
     window.addEventListener('hashchange', () => {
         let page = window.location.hash.replace('#', '') || 'home';
